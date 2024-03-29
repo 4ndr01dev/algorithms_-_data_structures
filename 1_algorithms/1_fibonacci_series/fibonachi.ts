86cbc3e99c81695ae2db429800a51842ce21f980
Alvaro Andrade Ardiles <dremian42@gmail.com>
2024-03-29 19:39:49 -0300

    feat ✨  (endpoints/auth): - fibonacci series Changes to be committed: 	modified:   1_algorithms/1_fibonacci_series/fibonachi.ts

--- 1_algorithms/1_fibonacci_series/fibonachi.ts
+++ 1_algorithms/1_fibonacci_series/fibonachi.ts
@@ -4,9 +4,16 @@ const getFibonacciSeries = (n: number) => {
     return fibonacciSeries
   }
   for (let i = 2; i < n; i++) {
-    fibonacciSeries = [...fibonacciSeries, i]
+    fibonacciSeries = [
+      ...fibonacciSeries,
+      fibonacciSeries[i - 1] + fibonacciSeries[i - 2],
+    ]
   }
   return fibonacciSeries
 }
 
-console.log(getFibonacciSeries(5))
+console.log('Fibonacci de 5 ', getFibonacciSeries(5))
+console.log('Fibonacci de 7 ', getFibonacciSeries(7))
+console.log('Fibonacci de 13 ', getFibonacciSeries(13))
+
+// Algorithmic complexity O(n) -linear 
